8026934cbdbae41f63d1f8962544c0c0488def43
George Filippakis <geo.filippakis@gmail.com>
2019-01-27 22:42:15 +0200

    SASS styles for Production environment

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,8 +1,40 @@
 const merge = require('webpack-merge');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
     mode: 'production',
     devtool: 'source-map',
+    plugins: [
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output. Both are optional
+            filename: '[name].css',
+            chunkFilename: '[id].css',
+        }),
+    ],
+    module: {
+        rules: [{
+            test: /\.scss$/,
+            use: [
+                MiniCssExtractPlugin.loader,
+                'css-loader', // translates CSS into CommonJS
+                'sass-loader', // compiles Sass to CSS, using Node Sass by default
+            ],
+        }],
+    },
+    // Setting optimization.minimizer in order to apply OptimizeCSSAssetsPlugin
+    // overrides the defaults provided by webpack, so need to also specify a JS minimizer
+    optimization: {
+        minimizer: [
+            new UglifyJsPlugin({
+                cache: true,
+                parallel: true,
+                sourceMap: true,
+            }),
+            new OptimizeCSSAssetsPlugin({}),
+        ],
+    },
 });
